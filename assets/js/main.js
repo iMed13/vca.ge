5dd2ab67433b1676ab8f4908811c06165ce62f36
imeda bekaia <36521268+iMed13@users.noreply.github.com>
2019-11-10 15:29:59 +0400

    mouse move add
    mouse move add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 
-$( document ).ready(function() {
     var width = $(".active-nav-link").outerWidth();
     var offset = $(".active-nav-link").position();
     var left= offset.left; 
@@ -38,27 +37,70 @@ $(".hamburger").click("on", function(){
   $(this).toggleClass('is-active')
 })
 
-});
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
 
-window.addEventListener('wheel', function(e)
-{
-  var secttion = $(".scrol-sec");
-  var lnnnn = secttion.length;
-  // console.log(secttion.eq(2).offset().top)
-  for(var i=0; i<=5; i++)
-  {
-    if(secttion.eq(i).offset().top == 0)
-    {
-      
-      $(".header-nav").find("li").removeClass("active-nav-link");
-      $(".header-nav").find("li").eq(i).addClass("active-nav-link");
-    }
-  }
 
-});
+
+$(".nav-ul").find('.nav-li').click("on", function(){
+  var width = $(".active-nav-link").outerWidth();
+  var offset = $(".active-nav-link").position();
+  var left= offset.left; 
+  $(".border-bottomm").css({"width" : width , "left" : left});
+
+  
+  $(this).siblings().removeClass('active-nav-link');
+  $(this).addClass('active-nav-link');
+  
+  $(".nav-ul.active-menu").removeClass('active-menu');
+  $(".hamburger.is-active").removeClass("is-active");
+})
+
+
+
+
+// Hmaburger click function,
+
+$('.hamburger').click('on', function(){
+  $(".nav-ul").toggleClass('active-menu');
+})
+
+
+//window resize function
+$( window ).resize(function() {
+  
+  var width = $(".active-nav-link").outerWidth();
+  var offset = $(".active-nav-link").position();
+  var left= offset.left; 
+  $(".border-bottomm").css({"width" : width , "left" : left});
+
+  var widthh = $( window ).width();
+  if( widthh >=767)
+  {
+    $(".nav-ul").removeClass('active-menu');
+    $(".hamburger").removeClass("is-active");
+
+  }
+})
+
+// on mouse scroll menu activing
+$(window).scroll(function() {
+  var scrollDistance = $(window).scrollTop() + 120;
+  var width = $(".active-nav-link").outerWidth();
+  var offset = $(".active-nav-link").position();
+  var left= offset.left; 
+  $(".border-bottomm").css({"width" : width , "left" : left});
+
+  $('.scrol-sec').each(function(i) {
+      if ($(this).offset().top <= scrollDistance) {
+        $('.header-nav').find(".nav-li").eq(i).siblings().removeClass("active-nav-link");
+        $('.header-nav').find(".nav-li").eq(i).addClass('active-nav-link');
+      }
+  });
+}).scroll();
+
 
